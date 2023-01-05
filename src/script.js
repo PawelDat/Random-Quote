34f9e5210e92b2cd06707f22b10724882db6061d
Pawel Datko <92950825+PawelDat@users.noreply.github.com>
2023-01-06 00:33:45 +0100

    api adjust

--- src/script.js
+++ src/script.js
@@ -38,7 +38,7 @@ function newQuote() {
 
 async function getQuotes() {
   loading();
-  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
